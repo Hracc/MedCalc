eddcb75bb91f9077890d3094faabfd2b25e50106
Hragun <115720772+Hracc@users.noreply.github.com>
2024-07-10 15:10:39 +0400

    test

--- src/main/java/com/pguti/med_calc/MedCalcApplication.java
+++ src/main/java/com/pguti/med_calc/MedCalcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MedCalcApplication {
-
+	//bruh
 	public static void main(String[] args) {
 		SpringApplication.run(MedCalcApplication.class, args);
 	}
