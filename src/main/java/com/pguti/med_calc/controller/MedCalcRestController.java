a97cd9e3844b00ff16a3f976abb69ee1060c2082
Hracc <egor.bazhenoff2016@yandex.ru>
2024-07-14 16:08:55 +0400

    bruh

--- src/main/java/com/pguti/med_calc/controller/MedCalcRestController.java
+++ src/main/java/com/pguti/med_calc/controller/MedCalcRestController.java
@@ -12,7 +12,6 @@ import java.util.Map;
 public class MedCalcRestController {
     private final MedCalcService medCalcService;
     @Autowired
-<<<<<<< HEAD
     public MedCalcRestController(MedCalcService medCalcService) {
         this.medCalcService = medCalcService;
     }
@@ -20,28 +19,10 @@ public class MedCalcRestController {
     public Map<String, String> getCalcList() {
         return medCalcService.getCalcList();
     }
-
     @GetMapping("/{calc}/info")
     public ResponseEntity<Map<String, String>> getInfo(@PathVariable String calc) {
         Map<String, String> calcInfo = medCalcService.getCalcInfo(calc);
         return ResponseEntity.ok(calcInfo);
-=======
-    private MedCalcService medCalcService;
-    @GetMapping("/calculator/{calc}/info")
-    public Map<String, String> getInfo(@PathVariable String calc){
-        return medCalcService.getCalcInfo(calc);
-    }
-    @PostMapping("/calculator/{calc}/result")
-    public Map<String, Object> getResult(@PathVariable String calc, @RequestBody Map<String, Object> params){
-        Map<String, Object> response = new HashMap<>();
-        try{
-            return medCalcService.getResult(calc, params);
-        } catch (IllegalArgumentException e) {
-            return Map.of("error", "Invalid calculator name or parameters: " + e.getMessage());
-        } catch (Exception e) {
-            return Map.of("error", "An unexpected error occurred: " + e.getMessage());
-        }
->>>>>>> parent of 8341e20 (попытки что то сделать)
     }
 
     @PostMapping("/{calc}/result")
