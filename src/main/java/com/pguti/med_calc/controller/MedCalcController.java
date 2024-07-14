0627556db98df58efb326473a6297ac09624940e
Hracc <egor.bazhenoff2016@yandex.ru>
2024-07-14 15:58:10 +0400

    bruh

--- src/main/java/com/pguti/med_calc/controller/MedCalcController.java
+++ src/main/java/com/pguti/med_calc/controller/MedCalcController.java
@@ -5,8 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MedCalcController {
+<<<<<<< HEAD
     @GetMapping("/")
     public String getMain(){
         return "index";
+=======
+    @Autowired
+    private MedCalcService medCalcService;
+
+
+    @GetMapping("/")
+    public String showCalcList(Model model){
+        model.addAttribute("calcList", medCalcService.getCalcList());
+        return "medCalcList";
+    }
+
+    @GetMapping("/{id}")
+    public String showCalc(@PathVariable String id, Model model){
+        model.addAttribute("calcList", medCalcService.getCalcById(id));
+
+        return "medCalc";
+>>>>>>> parent of 8341e20 (попытки что то сделать)
     }
 }
